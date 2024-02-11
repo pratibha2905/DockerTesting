6e6fd421bdcb1b5dd08460f4155c827841e2bb3c
ppandey200 <pratibha.pandey@dxc.com>
2024-02-11 13:07:02 +0530

    DockerCommit1

---
+++ src/main/java/com/docker/app/DockerDeployApp1Application.java
@@ -0,0 +1,13 @@
+package com.docker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDeployApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDeployApp1Application.class, args);
+	}
+
+}
